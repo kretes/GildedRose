47da62a2da79967d885eb9589b2c841656d3be6f
Tomasz Bartczak <tomasz.bartczak@pragmatists.pl>
2013-05-23 12:19:37 +0200

    added libs, PIT configuration, repackaged.

--- src/main/java/legacy/GildedRose.java
+++ src/main/java/legacy/GildedRose.java
@@ -1,18 +1,19 @@
+package legacy;
+
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class GildedRose {
 
-	private static List<Item> items = null;
+    private static List<Item> items = null;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
         System.out.println("OMGHAI!");
-		
+
         items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
@@ -22,15 +23,14 @@ public class GildedRose {
         items.add(new Item("Conjured Mana Cake", 3, 6));
 
         updateQuality();
-}
+    }
 
-
-	
     public static void updateQuality()
     {
         for (int i = 0; i < items.size(); i++)
         {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
+            if ((!"Aged Brie".equals(items.get(i).getName()))
+                    && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
             {
                 if (items.get(i).getQuality() > 0)
                 {
--- src/test/java/legacy/GildedRoseTest.java
+++ src/test/java/legacy/GildedRoseTest.java
@@ -1,12 +1,13 @@
+package legacy;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class GildedRoseTest {
 
-	@Test
-	public void testTheTruth() {
-		assertTrue(true);
-	}
+    @Test
+    public void testTheTruth() {
+        assertTrue(true);
+    }
 }
